58838f5cfcdb1012f1fab9648a979ceef7ac7da8
Amit Aswal <gametheory284@gmail.com>
2025-08-04 13:48:22 +0530

    Added Model,Data and Screen for Meals

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:meal_app/screens/categories.dart';
+import 'package:meal_app/screens/meals.dart';
+import 'package:meal_app/data/dummy_data.dart';
 
 
 final theme = ThemeData(
@@ -23,7 +25,7 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: theme,
-      home: CategoriesScreen(),
+      home: MealsScreen(title: 'Some Category', meals: dummyMeals),
     );
   }
 }
